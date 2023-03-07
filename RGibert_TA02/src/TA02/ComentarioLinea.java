efdaebb0ff65793a9d92aa8ca1254a7bd0598a8e
Roger <49531890+rogergibertrovira@users.noreply.github.com>
2023-03-07 11:29:50 +0100

    Fitxers tasca TA02

---
+++ RGibert_TA02/src/TA02/ComentarioLinea.java
@@ -0,0 +1,10 @@
+package TA02;
+
+public class ComentarioLinea {
+
+	public static void main(String[] args) {
+		//System.out.println("Hola");
+		System.out.println("Adeu");
+		System.out.println("FINAL DE PROGRAMA");
+	}
+}
